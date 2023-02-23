4ea9e485c21bdba1890da05ff88677f3f023b2e2
Erick Rick <erickcredjes@gmail.com>
2023-02-23 14:08:08 -0300

    Exercicio Filme

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const maps = require("gulp-sourcemaps");
+const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+
+function styles() {
+    return gulp.src("./src/styles/main.scss")
+        .pipe(maps.init())
+        .pipe( sass({ outputStyle: "compressed" }) )
+        .pipe(gulp.dest("./dist/css"));
+}
+
+function minificaImagem() {
+    return gulp.src("./src/images/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("./dist/images/"));
+}
+
+function scripts() {
+    return gulp.src("./src/scripts/main.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("./dist/scripts"));
+}
+
+exports.default = gulp.parallel([styles, minificaImagem, scripts]);
+exports.watch = function() {
+    gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+    gulp.watch("./src/images/**/*", gulp.parallel(minificaImagem));
+    gulp.watch("./src/scripts/*", gulp.parallel(scripts));
+}
